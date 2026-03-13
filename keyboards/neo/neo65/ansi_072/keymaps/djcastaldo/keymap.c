1a136ca93dd70828bbf4858f6105255001871abd
Dave Castaldo <djcastaldo@gmail.com>
2026-03-12 21:21:09 -0400

    added some additional _each functions to a few layer tap dances

--- keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
+++ keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [MAC_BASE] = LAYOUT_hot(
         DUAL_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, LT(EMO_LAYR,KC_BSLS), KC_GRV, KC_INS,
         LT(TMUX_LAYR,KC_TAB),   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,  BSPCFAST,  KC_PGUP,
-        TD(CAPS_LAYR),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,           KC_PGDN,
+        TD(CAPSFK_OSL),   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,           KC_PGDN,
         KC_LSFT,           KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    TD(RSFT_OSL),    KC_UP,  OSL_FKEY,
         KC_LCTL,  KC_LOPT,  KC_LCMD,                 KC_SPC,                TD(RCMD_OSL),  KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT),
 
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_hot(
         DUAL_ESC,  KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,  TD(DYN_LAYR), KC_GRV,  KC_INS,
         LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,    BSPCFAST,  KC_PGUP,
-        TD(CAPS_LAYR),     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,          KC_PGDN,
+        TD(CAPSFK_OSL),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,          KC_PGDN,
         KC_LSFT,            KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    TD(RSFT_OSL),   KC_UP,  OSL_FKEY,
         KC_LCTL, TD(LGUI_OSL), KC_LALT,              KC_SPC,              TD(RALT_OSL), KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT),
 
