3ea0d53f7c64a2c452bfdd784c53cfb169a6fb05
Dave Castaldo <djcastaldo@gmail.com>
2026-03-19 23:42:05 -0400

    add SP_RCTL keycode to allow using either right or left control with a single key

--- keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
+++ keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // : |________||____||____||____||____||____||____||____||____||____||____||____||____________||____| :
 // : |  LShift   || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  ||TD(RSFT) || up ||FKEY| :
 // : |___________||____||____||____||____||____||____||____||____||____||____||_________||____||____| :
-// : |LCtl  ||LOpt  ||LCmd ||                Space                ||TRCM ||RCtl |  | lt || dn || rt | :
+// : |LCtl  ||LOpt  ||LCmd ||                Space                ||TRCM ||SRCtl|  | lt || dn || rt | :
 // : |______||______||_____||_____________________________________||_____||_____|  |____||____||____| :
 // `--------------------------------------------------------------------------------------------------`
    [MAC_BASE] = LAYOUT_hot(
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(TMUX_LAYR,KC_TAB),   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,  BSPCFAST,  KC_PGUP,
         TD(CAPSFK_OSL),   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,           KC_PGDN,
         KC_LSFT,           KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    TD(RSFT_OSL),    KC_UP,  OSL_FKEY,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                 KC_SPC,                TD(RCMD_OSL),  KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                 KC_SPC,                TD(RCMD_OSL),  SP_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT),
 
 // [WIN_BASE]
 // .--------------------------------------------------------------------------------------------------.
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // : |________||____||____||____||____||____||____||____||____||____||____||____||____________||____| :
 // : |  LShift   || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  ||TD(RSFT) || up ||FKEY| :
 // : |___________||____||____||____||____||____||____||____||____||____||____||_________||____||____| :
-// : |LCtl  ||TDLGui||LAlt ||                Space                ||TRAlt||RCtl |  | lt || dn || rt | :
+// : |LCtl  ||TDLGui||LAlt ||                Space                ||TRAlt||SRCtl|  | lt || dn || rt | :
 // : |______||______||_____||_____________________________________||_____||_____|  |____||____||____| :
 // `--------------------------------------------------------------------------------------------------`
     [WIN_BASE] = LAYOUT_hot(
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,    BSPCFAST,  KC_PGUP,
         TD(CAPSFK_OSL),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,          KC_PGDN,
         KC_LSFT,            KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    TD(RSFT_OSL),   KC_UP,  OSL_FKEY,
-        KC_LCTL, TD(LGUI_OSL), KC_LALT,              KC_SPC,              TD(RALT_OSL), KC_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, TD(LGUI_OSL), KC_LALT,              KC_SPC,              TD(RALT_OSL), SP_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT),
 
 // [FKEY_LAYR]
 // .--------------------------------------------------------------------------------------------------.
