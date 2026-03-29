3c36390e7e9819bceaf5097e2d94958847d22e8e
Dave Castaldo <djcastaldo@gmail.com>
2026-03-29 17:12:50 -0400

    add RSFT_TD, a keycode with the functionality of tap dance RSFT_OSL, but works better over RDP connections

--- keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
+++ keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // : |_______||____||____||____||____||____||____||____||____||____||____||____||____||_______||____| :
 // : |TD(CAPS)|| A  || S  || D  || F  || G  || H  || J  || K  || L  || ;  || '  ||   Enter    ||PgDn| :
 // : |________||____||____||____||____||____||____||____||____||____||____||____||____________||____| :
-// : |  LShift   || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  ||TD(RSFT) || up ||FKEY| :
+// : |  LShift   || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  ||RSFT_TD  || up ||FKEY| :
 // : |___________||____||____||____||____||____||____||____||____||____||____||_________||____||____| :
 // : |LCtl  ||LOpt  ||LCmd ||                Space                ||TRCM ||SRCtl|  | lt || dn || rt | :
 // : |______||______||_____||_____________________________________||_____||_____|  |____||____||____| :
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         DUAL_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, LT(EMO_LAYR,KC_BSLS), KC_GRV, KC_INS,
         LT(TMUX_LAYR,KC_TAB),   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,  BSPCFAST,  KC_PGUP,
         TD(CAPSFK_OSL),   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,           KC_PGDN,
-        KC_LSFT,           KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    TD(RSFT_OSL),    KC_UP,  OSL_FKEY,
+        KC_LSFT,           KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    RSFT_TD,         KC_UP,  OSL_FKEY,
         KC_LCTL,  KC_LOPT,  KC_LCMD,                 KC_SPC,                TD(RCMD_OSL),  SP_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT),
 
 // [WIN_BASE]
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // : |_______||____||____||____||____||____||____||____||____||____||____||____||____||_______||____| :
 // : |TD(CAPS)|| A  || S  || D  || F  || G  || H  || J  || K  || L  || ;  || '  ||   Enter    ||PgDn| :
 // : |________||____||____||____||____||____||____||____||____||____||____||____||____________||____| :
-// : |  LShift   || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  ||TD(RSFT) || up ||FKEY| :
+// : |  LShift   || Z  || X  || C  || V  || B  || N  || M  || ,  || .  || /  ||RSFT_TD  || up ||FKEY| :
 // : |___________||____||____||____||____||____||____||____||____||____||____||_________||____||____| :
 // : |LCtl  ||TDLGui||LAlt ||                Space                ||TRAlt||SRCtl|  | lt || dn || rt | :
 // : |______||______||_____||_____________________________________||_____||_____|  |____||____||____| :
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         DUAL_ESC,  KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,   DYN_LT,      KC_GRV,  KC_INS,
         LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,    BSPCFAST,  KC_PGUP,
         TD(CAPSFK_OSL),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,          KC_PGDN,
-        KC_LSFT,            KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    TD(RSFT_OSL),   KC_UP,  OSL_FKEY,
+        KC_LSFT,            KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    RSFT_TD,        KC_UP,  OSL_FKEY,
         KC_LCTL, TD(LGUI_OSL), KC_LALT,              KC_SPC,              TD(RALT_OSL), SP_RCTL,         KC_LEFT, KC_DOWN, KC_RGHT),
 
 // [FKEY_LAYR]
