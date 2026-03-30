63cbd53a9622cee8e237b402c0b7dac5ec6d05f4
Dave Castaldo <djcastaldo@gmail.com>
2026-03-30 19:48:43 -0400

    updates to change key tapped for DYN_LT per keymap.c using get_dyn_ltkey()

--- keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
+++ keyboards/neo/neo65/ansi_072/keymaps/djcastaldo/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // [MAC_BASE]
 // .--------------------------------------------------------------------------------------------------.
 // :  ______________________________________________________________________________________________  :
-// : |DEsc|| 1  || 2  || 3  || 4  || 5  || 6  || 7  || 8  || 9  || 0  || -  || =  || L\ || `~ ||Ins | :
+// : |DEsc|| 1  || 2  || 3  || 4  || 5  || 6  || 7  || 8  || 9  || 0  || -  || =  || \  ||LT`~||Ins | :
 // : |____||____||____||____||____||____||____||____||____||____||____||____||____||____||____||____| :
 // : |LT-TMUX|| Q  || W  || E  || R  || T  || Y  || U  || I  || O  || P  || [  || ]  ||BspFast||PgUp| :
 // : |_______||____||____||____||____||____||____||____||____||____||____||____||____||_______||____| :
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // : |______||______||_____||_____________________________________||_____||_____|  |____||____||____| :
 // `--------------------------------------------------------------------------------------------------`
    [MAC_BASE] = LAYOUT_hot(
-        DUAL_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, LT(EMO_LAYR,KC_BSLS), KC_GRV, KC_INS,
+        DUAL_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, LT(EMO_LAYR,KC_GRV), KC_INS,
         LT(TMUX_LAYR,KC_TAB),   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,  BSPCFAST,  KC_PGUP,
         TD(CAPSFK_OSL),   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,           KC_PGDN,
         KC_LSFT,           KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    RSFT_TD,         KC_UP,  OSL_FKEY,
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // [WIN_BASE]
 // .--------------------------------------------------------------------------------------------------.
 // :  ______________________________________________________________________________________________  :
-// : |DEsc|| 1  || 2  || 3  || 4  || 5  || 6  || 7  || 8  || 9  || 0  || -  || =  ||DyLT|| `~ ||Ins | :
+// : |DEsc|| 1  || 2  || 3  || 4  || 5  || 6  || 7  || 8  || 9  || 0  || -  || =  || \  ||DYNL||Ins | :
 // : |____||____||____||____||____||____||____||____||____||____||____||____||____||____||____||____| :
 // : |LT-TMUX|| Q  || W  || E  || R  || T  || Y  || U  || I  || O  || P  || [  || ]  ||BspFast||PgUp| :
 // : |_______||____||____||____||____||____||____||____||____||____||____||____||____||_______||____| :
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // : |______||______||_____||_____________________________________||_____||_____|  |____||____||____| :
 // `--------------------------------------------------------------------------------------------------`
     [WIN_BASE] = LAYOUT_hot(
-        DUAL_ESC,  KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,   DYN_LT,      KC_GRV,  KC_INS,
+        DUAL_ESC,  KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,  KC_BSLS,      DYN_LT,  KC_INS,
         LT(TMUX_LAYR,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,  KC_LBRC,  KC_RBRC,    BSPCFAST,  KC_PGUP,
         TD(CAPSFK_OSL),    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,          KC_PGDN,
         KC_LSFT,            KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,    RSFT_TD,        KC_UP,  OSL_FKEY,
@@ -348,4 +348,8 @@ void leader_end_user(void) {
     }
 
     is_in_leader_sequence = false;
+}
+
+uint16_t get_dyn_ltkey(void) {
+    return KC_GRV;
 }
